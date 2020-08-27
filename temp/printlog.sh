362fdf5d30b9fbf0b6734aeb68884f28fd0f836e
OUSINN <ousinnkunn@gmail.com>
2020-08-28 00:05:02 +0900

    add temp file

---
+++ temp/printlog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#log path is /var/log/openstack-kilo
+#if commod execute sucessed,it will return 0 else return 1
+# Copyright 2014 Intel Corporation, All Rights Reserved.
+function log_info ()
+{
+if [  -d /var/log  ]
+then
+    mkdir -p /var/log 
+fi
+
+DATE_N=`date "+%Y-%m-%d %H:%M:%S"`
+USER_N=`whoami`
+echo "${DATE_N} ${USER_N} execute $0 [INFO] $@" >>/var/log/openstack-kilo #执行成功日志打印路径
+
+}
+
+function log_error ()
+{
+DATE_N=`date "+%Y-%m-%d %H:%M:%S"`
+USER_N=`whoami`
+echo -e "error ${DATE_N} ${USER_N} execute $0 [ERROR] $@ "  >>/var/log/openstack-kilo #执行失败日志打印路径
+
+}
+
+function fn_log ()  {
+if [  $? -eq 0  ]
+then
+    log_info "$@ sucessed."
+    echo -e "exec $@ sucessed. "
+else
+    log_error "$@ failed."
+    echo -e "exec $@ failed. "
+    exit 1
+fi
+}
+trap 'fn_log "DO NOT SEND CTR + C WHEN EXECUTE SCRIPT !!!! "'  2
---
+++ temp/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /home/wang/work/printlog.sh
+
+echo 'exec log_info printlog'
+log_info
+
+echo 'exec log_error printlog'
+log_error
+echo 'exec over'
