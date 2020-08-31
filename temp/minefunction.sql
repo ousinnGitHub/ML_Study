c016e2873688669836fa465b790f3840998d4055
OUSINN <ousinnkunn@gmail.com>
2020-09-01 01:12:50 +0900

    test func

---
+++ temp/minefunction.sql
@@ -0,0 +1,21 @@
+create or replace function mycount()
+returns integer as $$
+
+declare
+ mysql text;
+ counts integer;
+
+begin
+CREATE TEMP TABLE log (ts timestamp DEFAULT clock_timestamp(), msg text);
+
+mysql:='select count(*) from people';
+
+insert into log(msg) values ('execute：' || mysql);
+execute mysql into counts;
+insert into log(msg) values ('execute over return count:' );
+
+COPY log TO PROGRAM 'more >> /home/wang/work/log/fun.log';
+return counts;
+end;
+
+$$ language plpgsql;
